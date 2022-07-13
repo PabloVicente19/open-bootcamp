ee37e9479e096506338db3d2ce201e52c341cc5a
Pablo Vicente <pablovicente19@hotmail.com>
2022-07-13 06:08:16 -0300

    pude usar java en vscode :D

---
+++ ejercicio_tema_ocho/ejercicio_tema_8.java
@@ -0,0 +1,9 @@
+package ejercicio_tema_ocho;
+
+import javax.sound.sampled.SourceDataLine;
+
+public class ejercicio_tema_8 {
+    public static void main(String[] args) {
+        System.out.println("HOLA");
+    }
+}
